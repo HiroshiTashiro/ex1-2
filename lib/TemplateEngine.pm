64ff89cc3852d43d47d71ca81d1490d239eda123
Hiroshi Tashiro <infinith4@gmail.com>
2012-08-08 13:15:37 +0900

    Update lib/TemplateEngine.pm

--- lib/TemplateEngine.pm
+++ lib/TemplateEngine.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 use File::Basename;
-
+use DateTime;
 
 sub new{
 	my ($class,%location)=@_;
@@ -18,29 +18,48 @@ sub render(%){
 	my $dirname = dirname $location{file};
 	my $new=$dirname."/temp.html";
 	my (%var)=%{$var};
-	
+	my $imagefilename = basename($var{image});
+	$imagefilename =~s/[^.]*$//;
+	$imagefilename =~s/.$//;
+
+	#現在の日付(時間ふくむ)
+	my $datetime = DateTime->now( time_zone => 'Asia/Tokyo' );
+
 	open( OLD, "< $location{file}" )|| die "ファイルを開けません :$!\n";
 	open( NEW, "> $new" );
-	while ( <OLD> ) {
-		
-		$_=~s/{% title %}/$var{title}/;
-		$_=~s/{% content %}/$var{content}/;
-		print NEW $_;
-		
-	}
 	
+	for my $key (keys %var){
+		$var{$key}=~s/&/&amp/g;
+		$var{$key}=~s/>/&gt/g;
+		$var{$key}=~s/</&lt/g;
+		$var{$key}=~s/"/&quot/g;
+	}
+
+	while ( <OLD> ) {
+
+		for my $key (keys %var){
+			if($_=~'image'){
+				$_=~s/{%\s$key\s%}/<img src="$var{$key}" alt="$imagefilename" title="$imagefilename">/;
+			}elsif($_=~'datetime'){
+				$_=~s/{%\sdatetime\s%}/$datetime/;
+			}else{
+				$_=~s/{%\s$key\s%}/$var{$key}/;
+			}
+		}
+		print NEW $_;
+	}
+
 
 	close( OLD );
 	close( NEW );
-
 	open( NEW, "< $new" );
 	while ( <NEW> ) {
 		print $_;
 	}
-	
+	close(NEW);
+
 	unlink $new;
-	return ;
+return ;
 }
 
 1;
-
