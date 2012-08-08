b411db28caf9f4cb9b642336022fd2de4ce98b9d
HiroshiTashiro <infinith4@gmail.com>
2012-08-08 12:09:29 +0900

    main.pl

---
+++ main.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -I./lib
+use strict;
+use warnings;
+use TemplateEngine;
+
+
+my $template = TemplateEngine->new( file => 'templates/main.html' );
+print $template->render({
+  title   => 'タイトル',
+  content => 'これはコンテンツです',
+  image => 'img/img01.gif',
+}); 
